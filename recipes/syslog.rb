14714f3a3bc7e3d5d589b5e4083b8d067389735b
Aleksandr Dorofeev <akaspin@users.noreply.github.com>
2018-08-30 14:31:31 +0200

    disable rsyslog (#1)

--- recipes/syslog.rb
+++ recipes/syslog.rb
@@ -1,6 +1,10 @@
-node.force_default['rsyslog']['preserve_fqdn'] = 'on'
+# node.force_default['rsyslog']['preserve_fqdn'] = 'on'
+# include_recipe 'rsyslog'
 
-include_recipe 'rsyslog'
+# we don't need syslog
+service "rsyslog" do
+  action [:stop, :disable]
+end
 
 node.force_default['logrotate']['global']['compress'] = true
 
